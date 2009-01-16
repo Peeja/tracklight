ce80c912bd3622d75ee63c0a679efa34b162f036
Peter Jaros <peeja@dropio.com>
2009-01-16 15:56:35 -0500

    Fetch tickets via AJAX, page by page.

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
+require 'lighthouse'
 
+Lighthouse.account = 'dropio'
+Lighthouse.token = '85d698469180f0c1b0bd9ba45a79b36b5f38e736'
+PROJECT_ID = 4623 unless defined?(PROJECT_ID)
+
+get '/tickets' do
+  page = params[:page] || 1
+  tickets = Lighthouse::Ticket.find(:all, :params => { :project_id => PROJECT_ID, :q => "responsible:me", :page => page })
+  tickets.map { |t| {
+    :id => t.id,
+    :title => t.title
+  } }.to_json
+end
--- public/javascript/tracklight.js
+++ public/javascript/tracklight.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  $("#icebox").append("<li>A Ticket</li>")
+  fetchTickets();
 });
+
+
+// Fetches all tickets starting with given page (by default, page 1).
+function fetchTickets(page) {
+  if (page == undefined) page = 1;
+  
+  getTicketsFromPage = arguments.callee
+  
+  $.getJSON("/tickets?page="+page, function(data) {
+    // Add the tickets to the Icebox.
+    $.each(data, function() {
+      $("#icebox").append("<li>"+this.title+"</li>");
+    });
+    
+    // Fetch more.
+    if (data.length > 0) getTicketsFromPage(page+1);
+  });
+}
