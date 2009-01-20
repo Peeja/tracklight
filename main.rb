4beb38710021e09f3aa5bc26c3a30410b5a8b469
Peter Jaros <peeja@dropio.com>
2009-01-20 15:03:12 -0500

    Make sorting stick.  Ticket info is no longer loaded; this remains to be re-implemented.

--- main.rb
+++ main.rb
@@ -1,11 +1,29 @@
 require 'rubygems'
 require 'sinatra'
-require 'json'
 require 'lighthouse'
 
 Lighthouse.account = 'dropio'
 Lighthouse.token = '85d698469180f0c1b0bd9ba45a79b36b5f38e736'
 PROJECT_ID = 4623 unless defined?(PROJECT_ID)
+TICKET_LISTS_FILE = File.dirname(__FILE__) + "/data/ticket_order.yml" unless defined?(TICKET_LISTS_FILE)
+
+helpers do
+  def load_lists
+    lists = YAML.load_file(TICKET_LISTS_FILE) rescue {}
+    lists = {} unless lists.is_a? Hash
+    lists
+  end
+  
+  def write_lists(lists)
+    File.open(TICKET_LISTS_FILE, 'w') do |file|
+      file.write lists.to_yaml
+    end
+  end
+  
+  def write_list(name, list)
+    write_lists load_lists.merge(name => list)
+  end
+end
 
 get '/tickets' do
   page = params[:page] || 1
@@ -14,4 +32,12 @@ get '/tickets' do
     :id => t.id,
     :title => t.title
   } }.to_json
+end
+
+get '/lists' do
+  load_lists.to_json
+end
+
+post '/lists/:list' do
+  write_list params[:list], [params["ticket[]"]].flatten.compact.map { |tid| tid.to_i }
 end
--- public/javascript/tracklight.js
+++ public/javascript/tracklight.js
@@ -1,25 +1,72 @@
 $(document).ready(function() {
-  $("#queue,#icebox").sortable({
-    connectWith: ["#queue", "#icebox"]
+  // function createTicket(ticketInfo) {
+  //   return $("<li>"+ticketInfo.title+"</li>").attr({class: 'ticket', id: "ticket_"+ticketInfo.id});
+  // }
+  // 
+  // // Fetches all tickets starting with given page (by default, page 1).
+  // function fetchTickets(page) {
+  //   if (page == undefined) page = 1;
+  // 
+  //   getTicketsFromPage = arguments.callee
+  // 
+  //   $.getJSON("/tickets?page="+page, function(data) {
+  //     // Add the tickets to the Icebox.
+  //     $.each(data, function() {
+  //       $("#icebox").append(createTicket(this));
+  //     });
+  // 
+  //     // Fetch more.
+  //     if (data.length > 0) getTicketsFromPage(page+1);
+  //   });
+  // }
+  
+  // IMPLEMENT:
+  function createTicket(id) {
+    // return $("<li>Loading...</li>").attr({id: "ticket_"+id, class: 'ticket loading'});
+    return $("<li>#"+id+"</li>").attr({id: "ticket_"+id, class: 'ticket'});
+  }
+  
+  $(".list").fn({
+    // Moves and creates tickets to match the given list of ticket ids.
+    // After update, the list has exactly those tickets whose ids are given,
+    // and in the given order.
+    update: function(ticket_ids) {
+      self = this;
+      $.each(ticket_ids, function(i, id) {
+        // Find or create ticket by id.
+        ticket = $("#ticket_"+id).oror(function() {
+          return createTicket(id);
+        });
+        
+        // Insert in the correct place.
+        if (i == 0) { ticket.prependTo(self); }
+        else        { ticket.insertAfter($(self).find(".ticket:eq("+(i-1)+")")[0]); }
+      });
+      // Remove any extra tickets.
+      $(self).find(".ticket:gt("+(ticket_ids.length-1)+")").remove();
+    },
+    save: function() {
+      ticket_ids = $(this).sortable("serialize")
+      $.post('/lists/'+$(this).attr('id'), ticket_ids);
+    }
+  }).sortable({
+      connectWith: [".list"],
+      update: function(e, ui) {
+        $(this).fn('save');
+      }
   });
-
-  fetchTickets();
+  
+  // Fetch lists
+  // Fetch tickets for lists
+  // Fetch all tickets and add extras to Icebox
+  
+  // Fetch lists
+  $.getJSON("/lists", function(lists) {
+    // Fetch tickets for lists
+    $.each(lists, function(list, ticket_ids) {
+      $("#"+list).fn('update', ticket_ids);
+    })
+  })
+  
+  // fetchTickets();
 });
-
-
-// Fetches all tickets starting with given page (by default, page 1).
-function fetchTickets(page) {
-  if (page == undefined) page = 1;
-  
-  getTicketsFromPage = arguments.callee
-  
-  $.getJSON("/tickets?page="+page, function(data) {
-    // Add the tickets to the Icebox.
-    $.each(data, function() {
-      $("#icebox").append("<li>"+this.title+"</li>");
-    });
-    
-    // Fetch more.
-    if (data.length > 0) getTicketsFromPage(page+1);
-  });
-}
